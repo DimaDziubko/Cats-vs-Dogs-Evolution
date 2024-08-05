be1245647f100976ac405c36616acffe615e3234
destrouo <destrouo@gmail.com>
2024-08-05 18:15:46 +0300

    bugs fixed, env, anims

--- Assets/_Game/UI/_Hud/WaveInfoPopup.cs
+++ Assets/_Game/UI/_Hud/WaveInfoPopup.cs
@@ -1,27 +1,34 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Game.UI._Hud
 {
     public class WaveInfoPopup : MonoBehaviour
     {
         [SerializeField] private TMP_Text _label;
+        [SerializeField] private Image _image;
+        [SerializeField] private Sprite _allWavesSprite;
+        [SerializeField] private Sprite _finalWaveSprite;
         [SerializeField] private WaveInfoPopupAnimation _animation;
-        
+
         public void ShowWave(int wave, int wavesCount)
         {
-            if(_label == null) return;
+            if (_label == null) return;
+            if (_image == null) return;
             if (wave < wavesCount - 1)
             {
                 _label.text = $"Wave {wave}";
-                _label.color = Color.white;
+                //_label.color = Color.white;
+                _image.sprite = _allWavesSprite;
             }
             else
             {
                 _label.text = $"Final wave";
-                _label.color = Color.red;
+                _image.sprite = _finalWaveSprite;
+                //_label.color = Color.red;
             }
-            if(_animation == null) return;
+            if (_animation == null) return;
             _animation.PlayAnimation();
         }
 
