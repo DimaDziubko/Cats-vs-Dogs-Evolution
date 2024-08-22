77d068009c758e8d55a6011c1a17ce37cd2598ae
DimaDziubko <109381740+DimaDziubko@users.noreply.github.com>
2024-08-22 15:28:16 +0300

    Migrations deleted

--- Assets/_Game/Core/Communication/StateMigrationManager.cs
+++ Assets/_Game/Core/Communication/StateMigrationManager.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using _Game.Core.Communication.Migrations;
-using _Game.Core.UserState;
 using _Game.Core.UserState._State;
 using Assets._Game.Core.Communication;
 
